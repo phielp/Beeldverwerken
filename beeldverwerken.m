83378210fe403bcd6a5324dde3aba2ff8737a0c6
nietsteven <stevendeweille@gmail.com>
2016-04-01 16:29:59 +0200

    doei

--- beeldverwerken.m
+++ beeldverwerken.m
@@ -1,8 +1,12 @@
 function beeldverwerken
-    pixelValue('kids.tif',127,50,'nearest')
+    a = imread('autumn.tif');
+    a = im2double(rgb2gray(a));
+    plot(profile(a, 100, 100, 120, 120, 100, 'linear'));
 end
+
 function color = pixelValue(image,x,y,method)
-    im = imread(image);
+    %im = imread(image);
+    im = image;
     [height,width] = size(im);
     if inImage(x,y,width,height)
         switch(method)
@@ -29,3 +33,13 @@ end
 function boolean = inImage(x1,y1,width,height)
     boolean = ((x1 <= width) && (y1 <= height) && (height > 1) && (width > 1));
 end
+
+function line = profile(image, x0, y0, x1, y1, n, method)
+    % profile of an image along straight line in n points
+    x = linspace(x0, x1, n); 
+    y = linspace(y0, y1, n);
+    for i = 1:length(x)
+        line(i) = pixelValue(image, x(i), y(i), method);
+    end
+end
+
